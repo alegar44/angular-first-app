513f4ddec1f64a8828bcfec137325672fc71f2f2
Alejandro <agarcia@prismhr.com>
2016-10-14 14:36:33 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+
+  // you can use internal templates and style if is not longer than 3 lines.
+  // That's a good practice
+  //(for multiple lines use ``)
+  template: ` 
+            <h1> Inline template</h1>
+            <fa-other></fa-other>
+            <fa-another>
+             <div>
+                    <h1>Hello!</h1>
+                    <p>World!</p>
+             </div>
+            </fa-another>
+             <fa-another>
+                     <p>Something else</p>
+             </fa-another>
+  `,
+  styles: [`
+        h1 {
+            color: red;
+        }
+   `]
+
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'I changed it!';
 }
